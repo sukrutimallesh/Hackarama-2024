1337897537a6f54a13fd2d18407677a58079b097
jaxonhill <jaxoncharleshill@gmail.com>
2024-01-27 13:15:42 -0800

    Add yahoo scraping for links

--- backend/yahoo.py
+++ backend/yahoo.py
@@ -2,15 +2,36 @@ import requests
 from bs4 import BeautifulSoup, Tag
 import pprint
 
+from classes.SeleniumScraper import SeleniumScraper
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+
 BASE_URL: str = "https://finance.yahoo.com"
 MAIN_URL: str = "https://finance.yahoo.com/news/"
 
 def main():
-    r = requests.get(MAIN_URL)
-    soup = BeautifulSoup(r.text, "html.parser")
+    # r = requests.get(MAIN_URL)
+    # soup = BeautifulSoup(r.text, "html.parser")
+
+    # Create the scraper instance and navigate to URL
+    scraper: SeleniumScraper = SeleniumScraper(MAIN_URL, True)
+
+    # Scroll to the bottom of the page 5 times
+    for i in range(5):
+        scraper.scrollToBottomAndWait()
+    
+    # Get the soup from the page
+    soup: BeautifulSoup = scraper.createSoup()
 
     # Get <div> article card elements from the main <div>
-    main_div: Tag = soup.select_one("#Main")
+    main_div = soup.select_one("#Main")
     article_card_div_elements: list[Tag] = list(main_div.select("li div.Cf"))
     
     links: list[str] = []
