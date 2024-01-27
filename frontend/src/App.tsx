f9f98110b58d634f050cd6e9fbe613fc982afc2f
jaxonhill <jaxoncharleshill@gmail.com>
2024-01-27 09:44:23 -0800

    Add component library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
+	const [count, setCount] = useState<number>(0);
+
 	return (
 		<div className="px-96 py-8 bg-zinc-200 min-w-full min-h-screen">
 			<div className="w-full h-32 bg-white shadow rounded-2xl p-4">
-				Test
+				<Button onClick={() => setCount(count + 1)}>
+					Count: {count}
+				</Button>
 			</div>
 		</div>
 	);
