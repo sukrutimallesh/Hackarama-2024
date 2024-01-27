ff89761efe97ac6a2aa8871ac866ca17f7bcd528
jaxonhill <jaxoncharleshill@gmail.com>
2024-01-27 14:39:06 -0800

    Add function to get info from individual Yahoo finance articles

--- backend/yahoo.py
+++ backend/yahoo.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup, Tag
 import pprint
+import time
 
 from classes.SeleniumScraper import SeleniumScraper
 
@@ -13,15 +14,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 
+
 BASE_URL: str = "https://finance.yahoo.com"
 MAIN_URL: str = "https://finance.yahoo.com/news/"
+TEST_URL: str = "https://finance.yahoo.com/news/covid-era-program-awash-fraud-141658128.html"
 
-def main():
-    # r = requests.get(MAIN_URL)
-    # soup = BeautifulSoup(r.text, "html.parser")
 
+def getLinksFromNewsPage() -> list[str]:
     # Create the scraper instance and navigate to URL
-    scraper: SeleniumScraper = SeleniumScraper(MAIN_URL, True)
+    scraper: SeleniumScraper = SeleniumScraper(MAIN_URL, False)
 
     # Scroll to the bottom of the page 5 times
     for i in range(5):
@@ -47,7 +48,46 @@ def main():
                 links.append(BASE_URL + link_a_element["href"])
                 break
 
-    pprint.pprint(links)
+    return links
+
+
+def getInfoFromArticle(url: str) -> dict[str, str | list[str]]:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    # Find the title element and get its text
+    title_element: Tag = soup.select_one("#caas-lead-header-undefined")
+    title: str = title_element.text
+
+    # Find the image element and get its src url (it's always part of a figure)
+    img_element: Tag = soup.select_one(".caas-figure img.caas-img")
+    image_url: str = img_element["src"]
+
+    # Find the element that holds the main content
+    main_content_element: Tag = soup.select_one(".caas-body")
+
+    # Get every <p> element for only text
+    p_elements: list[Tag] = list(main_content_element.select("p"))
+
+    # Join all the <p> elements' text with newlines to create the final text content
+    text_content: str = "\n".join([p.getText() for p in p_elements])
+
+    return {
+        "title": title,
+        "article_url": url,
+        "image_url": image_url,
+        "tags": [],     # Empty tags for now
+        "article_content": text_content,
+    }
+
+
+def main():
+    # links: list[str] = getLinksFromNewsPage(scraper)
+    # pprint.pprint(links)
+
+    info = getInfoFromArticle(TEST_URL)
+    pprint.pprint(info)
+
 
 if __name__ == "__main__":
     main()
