e31f74de12dd2bd56de85b597e5466dacc083a3b
jaxonhill <jaxoncharleshill@gmail.com>
2024-01-27 17:41:14 -0800

    Add hover state to ArticleCard

--- frontend/src/components/ArticleCard.tsx
+++ frontend/src/components/ArticleCard.tsx
@@ -6,18 +6,20 @@ interface ArticleCardProps {
 
 export default function ArticleCard({ article }: ArticleCardProps) {
 	return (
-		<a href={article.article_url}>
-			<div className="bg-white shadow rounded-lg flex flex-col gap-4 overflow-hidden h-full w-full">
+		<a className="hover:cursor-pointer group" href={article.article_url}>
+			<div className="bg-white shadow rounded-lg flex flex-col gap-4 overflow-hidden h-full w-full group-hover:bg-zinc-100">
 				{article.image_url ? (
 					<img
-						className="aspect-video object-cover w-full"
+						className="aspect-video object-cover w-full group-hover:"
 						src={article.image_url}
 						alt={article.title}
 					/>
 				) : (
 					<div className="aspect-video object-cover w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500"></div>
 				)}
-				<h2 className="font-bold px-4 pb-4">{article.title}</h2>
+				<h2 className="font-bold px-4 pb-4 group-hover:text-zinc-800">
+					{article.title}
+				</h2>
 			</div>
 		</a>
 	);
