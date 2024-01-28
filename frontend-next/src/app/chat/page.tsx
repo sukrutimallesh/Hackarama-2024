fa53c03d2f8e5ea55afb8d1a8bd5ed0aa07b197a
jaxonhill <jaxoncharleshill@gmail.com>
2024-01-28 02:19:59 -0800

    Add text to ChatSection

--- frontend-next/src/app/chat/page.tsx
+++ frontend-next/src/app/chat/page.tsx
@@ -1,10 +1,18 @@
 import ChatSection from "@/components/ChatSection";
 
+const lorem =
+	"Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, nulla voluptate praesentium reiciendis unde iste, dolorum vero excepturi accusamus velit numquam pariatur voluptatem enim laborum alias. Debitis itaque pariatur sapiente? Neque officiis, obcaecati aliquam perferendis ex eligendi eius dicta commodi a veniam voluptatem fugiat esse in quaerat magnam, laborum ducimus voluptatibus illum autem, animi quasi necessitatibus impedit itaque? Vel, dolor? Voluptatum dignissimos quod magni, iusto quidem, eligendi vitae rem quae, asperiores ducimus ea illum reiciendis. Quis perspiciatis non assumenda iure quas excepturi laboriosam eligendi, rem doloribus velit consequuntur ratione est! Quidem quisquam at error aperiam dignissimos atque odit ullam tempora aut eligendi mollitia aliquid nobis adipisci minima, ex asperiores voluptatem fuga. Maxime quos asperiores consequatur illo aliquam, eum iure placeat! Quasi sunt voluptas esse cum, qui enim error. Rerum architecto optio error facere possimus nisi mollitia quod tempora nulla sint? Et ab neque, accusantium nam dicta facilis laudantium veritatis quos.";
+
 export default function ChatPage() {
 	return (
 		<main className="flex flex-col gap-12">
-			<ChatSection isAI={true} />
-			<ChatSection isAI={false} />
+			<ChatSection
+				isAI={true}
+				text={
+					"Hello! You can ask me anything about recent financial news and stocks! What would you like to know?"
+				}
+			/>
+			<ChatSection isAI={false} text={lorem} />
 		</main>
 	);
 }
--- frontend-next/src/components/ChatSection.tsx
+++ frontend-next/src/components/ChatSection.tsx
@@ -1,6 +1,12 @@
 import { Bot, UserRound } from "lucide-react";
 
-export default function ChatSection({ isAI }: { isAI: boolean }) {
+export default function ChatSection({
+	isAI,
+	text,
+}: {
+	isAI: boolean;
+	text: string;
+}) {
 	return (
 		<div className="gap-x-4 gap-y-2 grid grid-cols-16 items-center">
 			{isAI ? (
@@ -11,28 +17,7 @@ export default function ChatSection({ isAI }: { isAI: boolean }) {
 			<span className="font-bold col-start-2 col-span-full">
 				{isAI ? "NewsBot" : "You"}
 			</span>
-			<p className="col-start-2 col-span-full">
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem
-				reiciendis dignissimos molestiae laborum error reprehenderit?
-				Molestiae accusamus harum, ratione, omnis veniam ut illum
-				doloremque tempore eaque nam facilis, voluptas officiis? In
-				tempora natus commodi debitis, non quia tenetur ullam architecto
-				repellendus incidunt neque veniam praesentium nesciunt
-				voluptatibus deserunt sed esse? Nesciunt delectus cum ipsum
-				possimus praesentium, deserunt fuga debitis nobis. Eveniet
-				officiis quia cum. Veritatis tempore hic mollitia,
-				necessitatibus aperiam dolorem. Illo, impedit rem. Ratione at
-				adipisci, eos beatae voluptatem similique molestias excepturi
-				iste exercitationem odio iusto. Quasi, blanditiis optio! Saepe
-				itaque expedita aperiam est vitae ut dolorum, tempore
-				consequuntur qui aliquam excepturi molestiae officiis vero rem
-				corrupti ex eaque perferendis explicabo minus. Non deserunt,
-				provident ea commodi ipsum nisi. Ut, veritatis laboriosam.
-				Assumenda dolore similique perspiciatis temporibus voluptatibus
-				reprehenderit ea pariatur exercitationem esse numquam,
-				laboriosam omnis beatae architecto eius est voluptate ducimus
-				incidunt repellat et suscipit aliquam vero. Eum.
-			</p>
+			<p className="col-start-2 col-span-full">{text}</p>
 		</div>
 	);
 }
