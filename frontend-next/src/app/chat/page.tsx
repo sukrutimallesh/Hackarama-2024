0509eea1cf57a40a2fe32c261552efd218686e86
jaxonhill <jaxoncharleshill@gmail.com>
2024-01-28 02:16:27 -0800

    Add ChatSection component

--- frontend-next/src/app/chat/page.tsx
+++ frontend-next/src/app/chat/page.tsx
@@ -1,7 +1,10 @@
+import ChatSection from "@/components/ChatSection";
+
 export default function ChatPage() {
 	return (
-		<main className="flex flex-col gap-8">
-			<div></div>
+		<main className="flex flex-col gap-12">
+			<ChatSection isAI={true} />
+			<ChatSection isAI={false} />
 		</main>
 	);
 }
